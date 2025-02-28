8e183e0453a4774c3b0eb872611dc74248ebbbb9
Tarikul-Islam-Shykat <shykatislam138@gmail.com>
2025-02-28 18:20:04 +0600

    tarikul-local-feature

---
+++ lib/features/local/local_feature.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/main.dart';
+
+class LocalFeature extends StatefulWidget {
+  const LocalFeature({super.key});
+
+  @override
+  State<LocalFeature> createState() => _LocalFeatureState();
+}
+
+class _LocalFeatureState extends State<LocalFeature> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
